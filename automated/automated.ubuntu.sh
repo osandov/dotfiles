ddf66c8b651f8fd8a9c7f154dacff483cb0f3fd6
Omar Sandoval <osandov@cs.washington.edu>
2013-08-26 14:10:42 -0700

    update.sh now restarts if updating the repo changes it

--- automated/automated.ubuntu.sh
+++ automated/automated.ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install conky curl git hsetroot libghc-xmonad-contrib libxft-dev libxinerama-dev libxrandr-dev numlockx suckless-tools thunar trayer vim-gtk xclip xcompmgr xfce4-screenshooter xmonad zsh
+sudo apt-get install conky curl git hsetroot libghc-xmonad-contrib-dev libxft-dev libxinerama-dev libxrandr-dev numlockx suckless-tools thunar trayer vim-gtk xclip xcompmgr xfce4-screenshooter xmonad zsh
 # sudo apt-get install thunar xfce4-volumed shimmer-themes
--- update.sh
+++ update.sh
@@ -1,45 +1,53 @@
 #!/bin/sh
 
-echo "Updating top-level"
 cd ~/.dotfiles
+
+OLD_COMMIT=$(git log -1 --pretty="%H" update.sh)
+echo "Updating top-level"
 git pull
+NEW_COMMIT=$(git log -1 --pretty="%H" update.sh)
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-
-cd ~/.vim/autoload
-if [ -e pathogen.vim ]; then
-    echo "Updating pathogen"
+if [ $OLD_COMMIT != $NEW_COMMIT ]; then
+    echo "update.sh has changed, running the new one instead"
+    ./update.sh
 else
-    echo "Installing pathogen"
-fi
-curl -Sso pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
 
-cd ~/.vim/bundle
-if [ -d vim-colors-solarized ]; then
-    echo "Updating Vim Solarized"
-    cd vim-colors-solarized
-    git pull
-else
-    echo "Installing Vim Solarized"
-    git clone https://github.com/altercation/vim-colors-solarized.git
-fi
+    cd ~/.vim/autoload
+    if [ -e pathogen.vim ]; then
+        echo "Updating pathogen"
+    else
+        echo "Installing pathogen"
+    fi
+    curl -Sso pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-cd ~/.vim/bundle
-if [ -d nerdcommenter ]; then
-    echo "Updating NERD Commenter"
-    cd nerdcommenter
-    git pull
-else
-    echo "Installing NERD Commenter"
-    git clone https://github.com/scrooloose/nerdcommenter.git
-fi
+    cd ~/.vim/bundle
+    if [ -d vim-colors-solarized ]; then
+        echo "Updating Vim Solarized"
+        cd vim-colors-solarized
+        git pull
+    else
+        echo "Installing Vim Solarized"
+        git clone https://github.com/altercation/vim-colors-solarized.git
+    fi
 
-cd ~/.vim/bundle
-if [ -d vim-rsi ]; then
-    echo "Updating rsi.vim"
-    cd vim-rsi
-    git pull
-else
-    echo "Installing rsi.vim"
-    git clone https://github.com/tpope/vim-rsi.git
+    cd ~/.vim/bundle
+    if [ -d nerdcommenter ]; then
+        echo "Updating NERD Commenter"
+        cd nerdcommenter
+        git pull
+    else
+        echo "Installing NERD Commenter"
+        git clone https://github.com/scrooloose/nerdcommenter.git
+    fi
+
+    cd ~/.vim/bundle
+    if [ -d vim-rsi ]; then
+        echo "Updating rsi.vim"
+        cd vim-rsi
+        git pull
+    else
+        echo "Installing rsi.vim"
+        git clone https://github.com/tpope/vim-rsi.git
+    fi
 fi
