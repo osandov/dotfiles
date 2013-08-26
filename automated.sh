9d523a0b3fd8153980e55506847e87003888e74d
Omar Sandoval <osandov@cs.washington.edu>
2013-08-26 16:19:26 -0700

    Fix a bunch of portability issues

--- automated.sh
+++ automated.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-~/.dotfiles/install.sh
+cd "$(dirname "$0")"
+
+if [ "$(pwd)" != "$HOME/.dotfiles" ]
+then
+    echo 'This repo should be installed in ~/.dotfiles' >&2
+    exit 1
+fi
 
 if [ ! -z "$DISTRO" -a -x "$HOME/.dotfiles/automated/automated.$DISTRO.sh" ]; then
     "$HOME/.dotfiles/automated/automated.$DISTRO.sh"
 fi
+
+~/.dotfiles/install.sh
 
 git clone https://github.com/osandov/dzen.git /tmp/dzen
 cd /tmp/dzen
--- automated/automated.ubuntu.sh
+++ automated/automated.ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install conky curl git hsetroot libghc-xmonad-contrib-dev libxft-dev libxinerama-dev libxrandr-dev numlockx suckless-tools thunar trayer vim-gtk xclip xcompmgr xfce4-screenshooter xmonad zsh
-# sudo apt-get install thunar xfce4-volumed shimmer-themes
+sudo apt-get install conky curl git hsetroot libghc-xmonad-contrib-dev \
+    libxft-dev libxinerama-dev libxrandr-dev numlockx shimmer-themes \
+    suckless-tools thunar trayer vim-gtk volumeicon-alsa xclip xcompmgr \
+    xfce4-notifyd xfce4-power-manager xfce4-screenshooter xmonad zsh
