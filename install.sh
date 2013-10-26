6259697184d71d245ba04719df09201e1b2af537
Omar Sandoval <osandov@cs.washington.edu>
2013-10-26 01:25:24 -0700

    Update install scripts to allow for selective installation of configuration

--- install.sh
+++ install.sh
@@ -2,42 +2,156 @@
 
 cd "$(dirname "$0")"
 
-if [ "$(pwd)" != "$HOME/.dotfiles" ]
-then
-    echo 'This repo should be installed in ~/.dotfiles' >&2
+if [ "$(pwd)" != ~/.dotfiles ]; then
+    echo "This repo should be installed in ~/.dotfiles" >&2
     exit 1
 fi
 
-mkdir -p ~/.vim/tmp
-mkdir -p ~/.vim/backup
+usage () {
+    USAGE_STRING="Usage: $0 [-vztpdgx] [-y | -n]
+Usage: $0 -h
 
-ln -ins ~/.dotfiles/xmonad ~/.xmonad
-ln -is  ~/.dotfiles/Xresources ~/.Xresources
+Optional installation:
+  -v    Don't install Vim config
+  -z    Don't install zsh config
+  -t    Don't install tmux config
+  -p    Don't install Python config
+  -d    Don't install dircolors config
+  -g    Don't install GTK config
+  -x    Don't install xmonad config
+  -w    Don't install Vimperator config
 
-ln -ins ~/.dotfiles/vim/plugin ~/.vim/plugin
-ln -ins ~/.dotfiles/vim/after ~/.vim/after
-ln -is  ~/.dotfiles/vimrc ~/.vimrc
-ln -is  ~/.dotfiles/gvimrc ~/.gvimrc
-ln -is  ~/.dotfiles/vimperatorrc ~/.vimperatorrc
+Prompts:
+  -y    Assume yes when prompted about overwriting a file
+  -n    Assume no when prompted about overwriting a file
 
-ln -ins ~/.dotfiles/zsh ~/.zsh
-ln -is  ~/.dotfiles/zshenv ~/.zshenv
-ln -is  ~/.dotfiles/zshrc ~/.zshrc
-ln -is  ~/.dotfiles/tmux.conf ~/.tmux.conf
+Miscellaneous:
+  -h    Display this help message and exit"
 
-ln -is  ~/.dotfiles/pythonrc ~/.pythonrc
-ln -is  ~/.dotfiles/dircolors ~/.dircolors
+    case "$1" in
+        out)
+            echo "$USAGE_STRING"
+            exit 0
+            ;;
+        err)
+            echo "$USAGE_STRING" >&2
+            exit 1
+            ;;
+    esac
+}
 
-mkdir -p ~/.config/gtk-3.0
-ln -is  ~/.dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
-ln -is  ~/.dotfiles/gtkrc-3.0 ~/.config/gtk-3.0/settings.ini
+while getopts ":vztpdgxwynh" OPT; do
+    case "$OPT" in
+        v)
+            NO_VIM=1
+            ;;
+        z)
+            NO_ZSH=1
+            ;;
+        t)
+            NO_TMUX=1
+            ;;
+        p)
+            NO_PYTHON=1
+            ;;
+        d)
+            NO_DIRCOLORS=1
+            ;;
+        g)
+            NO_GTK=1
+            ;;
+        x)
+            NO_XMONAD=1
+            ;;
+        w)
+            NO_VIMPERATOR=1
+            ;;
+        y)
+            ASSUME_ANSWER=y
+            ;;
+        n)
+            ASSUME_ANSWER=n
+            ;;
+        h)
+            usage "out"
+            ;;
+        *)
+            usage "err"
+            ;;
+    esac
+done
 
-case $DISTRO in
-    arch)
-        ln -is  ~/.dotfiles/xmonad/startxmonad ~/.xinitrc
-        ;;
-    ubuntu)
-        sudo ln -is ~/.dotfiles/xmonad/startxmonad /usr/bin/startxmonad
-        sudo cp -i ~/.dotfiles/xmonad/xmonad.desktop /usr/share/xsessions/
-        ;;
-esac
+install_file () {
+    case "$ASSUME_ANSWER" in
+        y)
+            ln -fns "$1" "$2"
+            ;;
+        n)
+            if [ -e "$2" ]; then
+                echo "$2 already exists; not installing" >&2
+            else
+                ln -nfs "$1" "$2"
+            fi
+            ;;
+        *)
+            ln -ins "$1" "$2"
+            ;;
+    esac
+}
+
+if [ -z "$NO_VIM" ]; then
+    mkdir -p ~/.vim/tmp
+    mkdir -p ~/.vim/backup
+
+    install_file ~/.dotfiles/vim/plugin ~/.vim/plugin
+    install_file ~/.dotfiles/vim/after ~/.vim/after
+    install_file ~/.dotfiles/vimrc ~/.vimrc
+    install_file ~/.dotfiles/gvimrc ~/.gvimrc
+fi
+
+if [ -z "$NO_ZSH" ]; then
+    install_file ~/.dotfiles/zsh ~/.zsh
+    install_file ~/.dotfiles/zshenv ~/.zshenv
+    install_file ~/.dotfiles/zshrc ~/.zshrc
+fi
+
+if [ -z "$NO_TMUX" ]; then
+    install_file ~/.dotfiles/tmux.conf ~/.tmux.conf
+fi
+
+if [ -z "$NO_PYTHON" ]; then
+    install_file ~/.dotfiles/pythonrc ~/.pythonrc
+fi
+
+if [ -z "$NO_DIRCOLORS" ]; then
+    install_file ~/.dotfiles/dircolors ~/.dircolors
+fi
+
+if [ -z "$NO_GTK" ]; then
+    mkdir -p ~/.config/gtk-3.0
+    install_file ~/.dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
+    install_file ~/.dotfiles/gtkrc-3.0 ~/.config/gtk-3.0/settings.ini
+fi
+
+if [ -z "$NO_XMONAD" ]; then
+    case "$DISTRO" in
+        arch)
+            install_file ~/.dotfiles/xmonad/startxmonad ~/.xinitrc
+            ;;
+        ubuntu)
+            sudo install_file ~/.dotfiles/xmonad/startxmonad /usr/bin/startxmonad
+            sudo install_file ~/.dotfiles/xmonad/xmonad.desktop /usr/share/xsessions/
+            ;;
+        *)
+            echo "unrecognized distro; not installing X configuration" >&2
+            exit 1
+            ;;
+    esac
+
+    install_file ~/.dotfiles/xmonad ~/.xmonad
+    install_file ~/.dotfiles/Xresources ~/.Xresources
+fi
+
+if [ -z "$NO_VIMPERATOR" ]; then
+    install_file ~/.dotfiles/vimperatorrc ~/.vimperatorrc
+fi
