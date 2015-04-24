1b8f01d4be078219f48982db983923b83cde1d60
Omar Sandoval <osandov@osandov.com>
2015-04-24 14:12:06 -0700

    Refactor automated Arch install script

--- automated/automated.arch.sh
+++ automated/automated.arch.sh
@@ -1,20 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # It is assumed that you have completed the install process, including
 # installing X and your video drivers and setting up your network connection
 
 set -e
 
-sudo pacman  -Sy
-sudo pacman --noconfirm -S --needed alsa-utils base-devel clang dmenu feh \
-	firefox git gvfs gvim numlockx openssh pkgfile python ristretto \
-	the_silver_searcher thunar thunar-volman tmux ttf-dejavu tumbler \
-	volumeicon xcursor-vanilla-dmz xfce4-notifyd xfce4-power-manager \
-	xfce4-screenshooter xdm-archlinux xorg-xdm xorg-xmessage xorg-xmodmap \
-	xorg-xrdb xorg-xrandr xorg-xset xorg-xsetroot xscreensaver xterm \
-	zathura zathura-pdf-poppler zsh
+sudo pacman -Sy
 
-sudo systemctl enable xdm-archlinux.service
+# Install yaourt manually from the AUR first.
+# sudo pacman --noconfirm -S --needed base-devel
 
 cd /tmp
 curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
@@ -30,7 +24,53 @@ cd yaourt
 makepkg -s
 sudo pacman --noconfirm -U yaourt-*.pkg.tar.xz
 
-yaourt --noconfirm -S compton-git conky-lua elementary-xfce-icons \
-    xfce-theme-greybird
+# Install the packages.
+PACKAGES=(
+	alsa-utils
+	clang
+	compton-git
+	conky-lua
+	dmenu
+	elementary-xfce-icons
+	feh
+	firefox
+	git
+	gvfs
+	gvim
+	numlockx
+	openssh
+	pkgfile
+	python
+	ristretto
+	the_silver_searcher
+	thunar
+	thunar-volman
+	tmux
+	ttf-dejavu
+	tumbler
+	volumeicon
+	xcursor-vanilla-dmz
+	xdm-archlinux
+	xfce4-notifyd
+	xfce4-power-manager
+	xfce4-screenshooter
+	xfce-theme-greybird
+	xorg-xdm
+	xorg-xmessage
+	xorg-xmodmap
+	xorg-xrandr
+	xorg-xrdb
+	xorg-xset
+	xorg-xsetroot
+	xscreensaver
+	xterm
+	zathura
+	zathura-pdf-poppler
+	zsh
+)
 
+yaourt --noconfirm -S --needed "${PACKAGES[@]}"
+
+# Post-install stuff.
+sudo systemctl enable xdm-archlinux.service
 sudo pkgfile --update
