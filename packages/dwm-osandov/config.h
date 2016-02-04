5560538a73da6174ffc343c3d785b284a0f04816
Omar Sandoval <osandov@osandov.com>
2016-02-04 15:37:55 -0800

    dwm: fix windows not opening on the selected monitor

--- packages/dwm-osandov/config.h
+++ packages/dwm-osandov/config.h
@@ -25,7 +25,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{NULL},
+	{  NULL,      NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
