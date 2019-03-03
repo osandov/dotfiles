7030491f0aac86974bca9d5edcba1af91af80d9c
Omar Sandoval <osandov@osandov.com>
2019-03-03 03:25:39 -0800

    Change to Black Lodge theme

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ fi
 
 if do_install "$DO_DESKTOP"; then
 	mkdir -p ~/.config
-	curl -Lo ~/.config/wallpaper.png 'https://www.dropbox.com/s/dszzy8ey0hc936l/penguin.png?raw=1'
+	curl -Lo ~/.config/wallpaper.png 'https://www.dropbox.com/s/62st3y31p3iz4os/blacklodge.png?raw=1'
 	install_file ~/.dotfiles/desktop/compton.conf ~/.config/compton.conf
 	install_file ~/.dotfiles/desktop/gtkrc-2.0 ~/.gtkrc-2.0
 	mkdir -p ~/.config/gtk-3.0
--- packages/dwm-osandov/config.h
+++ packages/dwm-osandov/config.h
@@ -11,11 +11,11 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_red[]        = "#5f0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_red,  col_red  },
 };
 
 /* tagging */
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", "new", ";", "set", "-q", "destroy-unattached", NULL };
 static const char *lockcmd[]  = { "xscreensaver-command", "-lock", NULL };
 static const char *screencap_full[]   = { "xfce4-screenshooter", "-f", NULL };
